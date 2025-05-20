cdcffbbe06bf693915a2c291a2a17bd320979233
G-C62 <nsw9211@gmail.com>
2025-05-20 23:41:23 +0900

    Apply layout components

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,15 +1,20 @@
-import BookSearchInput from "@/books/components/BookSearchInput";
+import { BookSearchInput } from "@/books/components/BookSearchInput";
 import BookSearchResult from "@/books/components/BookSearchResult";
 import BookSearchSummary from "@/books/components/BookSearchSummary";
+import { Space } from "@/components/Space";
 import { Title2 } from "@/components/Typography";
+import { VStack } from "@/components/VStack";
 
 export default function Books() {
   return (
-    <>
+    <VStack>
       <Title2>도서 검색</Title2>
-      <BookSearchInput/>
-      <BookSearchSummary/>
-      <BookSearchResult/>
-    </>
+      <Space height="16px" />
+      <BookSearchInput />
+      <Space height="25px" />
+      <BookSearchSummary />
+      <Space height="36px" />
+      <BookSearchResult />
+    </VStack>
   );
 }
